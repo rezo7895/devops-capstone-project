79c920a78564a78371b9de087d34141b09c912df
rezo7895 <alawa08@gmail.com>
2024-03-05 14:47:50 -0500

    read_update

--- service/routes.py
+++ service/routes.py
@@ -69,8 +69,14 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+@app.route("/accounts/<id>", methods=["GET"])
+def get_accounts(id):
 
-
+    app.logger.info("Request to read an Account with id: %s", id)
+    account = Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
